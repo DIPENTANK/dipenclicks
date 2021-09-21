841d955ceb7051b19d07dc23e9e510e322c9145e
DIPENTANK <dipentank5@gmail.com>
2021-09-21 14:49:23 +0530

    edit3

--- project/urls.py
+++ project/urls.py
@@ -5,10 +5,15 @@ from portfolio import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.home, name="home"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
