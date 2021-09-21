3b278d285f506a64794e9a7a5af2f825bc31b7ef
DIPENTANK <dipentank5@gmail.com>
2021-09-21 10:23:32 +0530

    'update3'

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-8ii@+*-0$sy(xf913jt4&sq1k+pawrj71qa06t^-0iqr6go_w+'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
